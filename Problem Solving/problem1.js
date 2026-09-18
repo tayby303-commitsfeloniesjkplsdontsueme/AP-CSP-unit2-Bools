b4109b81a13c35de090aa2309ebbda50a04e6825
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-09-18 14:02:30 -0400

    commit

--- Problem Solving/problem1.js
+++ Problem Solving/problem1.js
@@ -1,1 +1,17 @@
-function classifyNumber(num) {
+function classifyNumber(n) {
+    if(n===0)
+        console.log(`${n} is zero`);
+    else if (n>0 && n % 2 ===0)
+        console.log(`${n} is positive & even`);
+    else if (n>0 && n % 2 ===1)
+        console.log(`${n} is positive & odd`);
+    else if (n<0 && n % 2 ===0)
+        console.log(`${n} is negative & even`);
+    else
+        console.log(`${n} is negative & odd`);
+}
+classifyNumber(0)
+classifyNumber(4)
+classifyNumber(7)
+classifyNumber(-4)
+classifyNumber(-7)
